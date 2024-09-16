313c07ae5a568ffca8a5eb926a3cd8c43b2dc340
Gaurav Ghuge <guduughuge7@gmail.com>
2024-09-16 15:41:01 +0530

    solve the bug

--- day2_Operators/arithmeticOperator.java
+++ day2_Operators/arithmeticOperator.java
@@ -1,3 +1,4 @@
+package day3_Operators;
 public class arithmeticOperator {
     public static void main(String[] args) {
         int a =20;
---
+++ day2_Operators/demo.java
@@ -0,0 +1,22 @@
+package day3_Operators;
+class demo {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        // boolean name = false;
+
+    String name = unidentified;
+
+    // String name = unidentified;
+
+
+        if(!name) {
+            System.out.println(" this is the false value");
+        }
+
+
+
+
+    }
+}
